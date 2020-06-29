ecae685b2858ad72c95d0e1e7575301c76d9b2fc
winypr <63911228+winypr@users.noreply.github.com>
2020-06-30 00:57:48 +0900

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,8 @@
 
 (function($){
   $(function(){
-    $("li .submenu").click(function(){
+    $("li.submenu").click(function(){
+      console.log("테스트");
       if ($(this).hasClass('active')) $(this).removeClass('active');
       else $(this).addClass('active'); 
     });
