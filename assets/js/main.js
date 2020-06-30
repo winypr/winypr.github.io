efd0bbb9c1e37015e676abf6004746d0dd30ade3
winypr <63911228+winypr@users.noreply.github.com>
2020-06-30 23:41:28 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 (function($){
   $(function(){
     $(".button-collapse").sideNav();
+    var $dropDown = $(".dropdown-button > i");
+        $dropDown.eq(1).dropdown();
+        console.log("테스트");
   });
 })(jQuery);
