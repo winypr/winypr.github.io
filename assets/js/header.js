942de30fdae931368e5187acdc4f45d6ed023c0d
winypr <63911228+winypr@users.noreply.github.com>
2020-06-30 19:12:41 +0900

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,10 +1,6 @@
 
 (function($){
   $(function(){
-    $("li.submenu").click(function(){
-      console.log("테스트");
-      if ($(this).hasClass('active')) $(this).removeClass('active');
-      else $(this).addClass('active'); 
-    });
+   $(".dropdown-button").dropdown();
   });
 })(jQuery);
